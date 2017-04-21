60c9aa5df00be77d27a68ac9d3965a1267dccfa9
aptivator <aptivator@gmail.com>
2017-04-21 15:34:48 -0400

    Remove one-file promise-keeper

--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 let PromiseKeeper = require('../dist/promise-keeper');
 let {expect} = require('chai');
 
+let c = console.log.bind(console);
+
 describe('promise-keeper', () => {
   it('tests', done => {
-    done();
+    let promise = new PromiseKeeper(resolve => resolve(25));
+    promise.then(n => {throw n;}).then(n => {throw n;}).then(n => {throw n;}).then(n => {throw n;}).then(c, c).then(() => done());
   });  
 });
